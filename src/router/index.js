838a732d7648a3a511f46caad80ebf810c17feba
Anshari <kocortowap1@gmail.com>
2021-02-21 02:05:42 +0700

    add components

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,25 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../components/Home/LoginPendaftar')
+  },
+  {
+    path: '/reset-password',
+    name: 'resetPassword',
+    component: () => import(/* webpackChunkName: "resetPassword" */ '../components/Home/resetPassword')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import(/* webpackChunkName: "Register" */ '../views/Register.vue')
+  },
+
+  {
+    path : "*",
+    name: 'NotFound',
+    component: () => import(/* webpackChunkName: "404" */ '../views/404.vue')
   }
 ]
 
