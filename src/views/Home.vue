02f6a825b508e68bdc731caf9f1050d1f0b18bfb
Anshari <kocortowap1@gmail.com>
2021-02-21 02:06:07 +0700

    fixing home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <section class="my-0 ">
+      <Jumbotron />
+    </section>
+    <section class="my-md-2">
+      <Jalur />
+    </section>
+    <section class="my-0">
+      <Alur />
+    </section>
+    <section>
+      <Footer />
+    </section>
 
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Jalur from '../components/Home/jalur'
+import Alur from '../components/Home/alurReguler'
+import Footer from '../components/Home/Footer'
+import Jumbotron from '../components/Home/jumbotron'
 
 export default {
   name: 'Home',
   components: {
+    Jalur,
+    Alur,
+    Footer,
+    Jumbotron
   }
 }
 </script>
