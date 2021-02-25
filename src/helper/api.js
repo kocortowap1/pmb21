2034eaefaf4db004f1453d7a7defe4a1b26ea52a
Anshari <kocortowap1@gmail.com>
2021-02-26 02:49:21 +0700

    perbaikan api call

--- src/helper/api.js
+++ src/helper/api.js
@@ -3,8 +3,10 @@ import axios from 'axios'
 const baseUrl = process.env.NODE_ENV === 'production' ? 'https://api.pmb.unuja.ac.id' : '';
 axios.defaults.baseURL = baseUrl
 
+
+
 const setAuthorization = (token) => {
-    axios.defaults.headers['Authorization'] = `Bearer ${token}`
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
 }
 const removeAuthorization = () => {
     delete axios.defaults.headers['Authorizazion']
@@ -18,48 +20,61 @@ const isActiveSession = (token) => {
     const parsedToken = JSON.parse(atob(tokenBody))
     const now = new Date().getTime() / 1000;
     const selisih = parsedToken.exp - now
-    if(selisih > 1){
+    if (selisih > 1) {
         return true
     }
     return false
 }
 
 
-const login  = async (email, password) => {
-    let response = {status: false}
-    if(!email){
+const login = async (email, password) => {
+    let response = { status: false }
+    if (!email) {
         response['message'] = 'Email tidak boleh kosong'
     }
-    if(!password) {
+    if (!password) {
         response['message'] = 'Password tidak boleh kosong'
     }
-    const req = await axios.post('/auth', {email : email, password: password})
-    if(req.status === 200 && req.headers['x-token']){
-        setAuthorization(req.headers['x-token'])
+    const req = await axios.post('/register/login', { email: email, password: password })
+    if (req.status === 200 && req.headers['x-token']) {
+        // setAuthorization(req.headers['x-token'])
         response.status = true
         response.token = req.headers['x-token']
-    }else{
+    } else {
         response['message'] = 'Terjadi kesalahan, silakan coba lagi'
         removeAuthorization()
     }
     return response
 }
 
-const getPublicData = async(url) => {
-   const req = await axios.get(url)
-    return req
+const getPublicData = async (url) => {
+    let response = { status: false }
+    try {
+        const req = await axios.get(url)
+        // return req
+        if (!req.data || !Object.entries(req.data).length) {
+            response['message'] = "Data kosong"
+        } else {
+            response.status = true
+            response['data'] = req.data
+        }
+
+    } catch (error) {
+        response = error
+    }
+    return response
 }
-const postPublicData = async(url, data) => {
-    let response = {status: false}
+const postPublicData = async (url, data) => {
+    let response = { status: false }
     try {
         const req = await axios.post(url, data, {
             headers: {
-                'Content-Type' : 'application/json'
+                'Content-Type': 'application/json'
             }
         })
-        if(!req.data.status){
+        if (!req.data.status) {
             response['message'] = req.data.message
-        }else{
+        } else {
             // response.status = true
             // response['data'] = req.data
             response = req.data
@@ -70,28 +85,34 @@ const postPublicData = async(url, data) => {
     }
     return response
 }
-// const putPublicData = async(url, data) => {
-//     let response = {status: false}
-//     try {
-//         const req = await axios.put(url, data, {
-//             headers: {
-//                 'Content-Type' : 'application/json'
-//             }
-//         })
-//         if(req.status !== 200){
-//             response['message'] = req.data.message
-//         }else{
-//             response.status = true
-//             response['data'] = data
-//         }
 
-//     } catch (error) {
-//         response = error
-//     }
-//     return response
-// }
+const getPrivateData = async (url) => {
+    // console.log(axios.defaults)
+    // axios.defaults.headers.common
+    let response = { status: false }
+    try {
+        const req = await axios.get(url, { headers: { 'Authorization': `Bearer ${localStorage.getItem('b')}` } })
+        // return req
+        if (!req.data || !Object.entries(req.data).length) {
+            response['message'] = "Data kosong"
+        } else {
+            response.status = true
+            response['data'] = req.data
+        }
 
-
+    } catch (error) {
+        response = error
+    }
+    return response
+}
+const postPrivateData = async (url, data) => {
+    const req = await axios.post(url, data)
+    return req
+}
+const putPrivateData = async (url, data) => {
+    const req = await axios.put(url, data)
+    return req
+}
 export {
     setAuthorization,
     removeAuthorization,
@@ -100,5 +121,7 @@ export {
     login,
     getPublicData,
     postPublicData,
-
+    getPrivateData,
+    postPrivateData,
+    putPrivateData
 }
