02c8983972ddf66be37050e718300d1df06b01fa
Anshari <kocortowap1@gmail.com>
2021-02-26 02:47:16 +0700

    disable global instance bootstrap vue

--- src/main.js
+++ src/main.js
@@ -4,33 +4,33 @@ import router from './router'
 import store from './store'
 
 import {
-  NavbarPlugin,
-  NavPlugin,
-  DropdownPlugin,
-  LayoutPlugin,
-  CollapsePlugin,
-  FormPlugin,
-  FormInputPlugin,
-  FormDatepickerPlugin,
-  IconsPlugin,
-  ModalPlugin
+  // NavbarPlugin,
+  // NavPlugin,
+  // DropdownPlugin,
+  // LayoutPlugin,
+  // CollapsePlugin,
+  // FormPlugin,
+  // FormInputPlugin,
+  // FormDatepickerPlugin,
+  // IconsPlugin,
+  // ModalPlugin
 } from 'bootstrap-vue'
 // import  './scss/custom.scss'
 
 // import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-Vue.use(NavbarPlugin)
-Vue.use(NavPlugin)
-Vue.use(DropdownPlugin)
-Vue.use(LayoutPlugin)
-Vue.use(CollapsePlugin)
-Vue.use(FormPlugin)
-Vue.use(FormInputPlugin)
-Vue.use(FormDatepickerPlugin)
-Vue.use(IconsPlugin)
+// Vue.use(NavbarPlugin)
+// Vue.use(NavPlugin)
+// Vue.use(DropdownPlugin)
+// Vue.use(LayoutPlugin)
+// Vue.use(CollapsePlugin)
+// Vue.use(FormPlugin)
+// Vue.use(FormInputPlugin)
+// Vue.use(FormDatepickerPlugin)
+// Vue.use(IconsPlugin)
 
-Vue.use(ModalPlugin )
+// Vue.use(ModalPlugin )
 
 Vue.config.productionTip = false
 
