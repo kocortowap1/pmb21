4cb61aa06736698fc8bdd0c6ca57bf1a67c0c5f3
Anshari <kocortowap1@gmail.com>
2021-12-30 15:24:39 +0700

    move to tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    display: ['responsive', 'group-hover', 'group-focus'],
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss/colors')
+  ],
+}
