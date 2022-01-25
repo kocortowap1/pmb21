f3e2c7e392357713a4a46308f563d8c7663ee8c0
Anshari <kocortowap1@gmail.com>
2022-01-25 09:45:54 +0700

    add stepDaftar

---
+++ src/components/Home/StepDaftar.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <ul>
+        <li>Membuat Akun</li>
+        <li>Melengkapi Data</li>
+        <li>Unggah Unggah Berkas</li>
+        <li>Mengisi Kuisioner</li>
+        <li>Membayar Pendaftaran</li>
+        <li>Mengikuti Seleksi</li>
+        <li>Melihat Hasil Seleksi</li>
+        <li>Daftar Ulang</li>
+        <li>Kuliah</li>
+    </ul>
+</div>
+</template>
